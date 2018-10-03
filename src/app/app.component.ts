7dab9428dd371894fa7eca4b1c1a263cbb9f6e91
Felipe Panegalli <felipe.panegalli@gmail.com>
2018-10-03 09:52:46 -0300

    Login em andamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    // 'Content-Type': 'application/json'
+    'Content-Type': 'application/json'
+  })
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ConnectApi';
+  public webPathAPI =
+    'https://cors-anywhere.herokuapp.com/http://apirest.esy.es/api/v1/users/login.json';
+  public localPathAPI = 'http://localhost:8765/api/v1/users/login.json';
+  public usuario = {
+    username: 'teste',
+    password: 123456
+  };
+
+  constructor(public http: HttpClient) {}
+
+  onClick() {
+    console.log('clicou em login');
+    this.http
+      .post(this.webPathAPI, JSON.stringify(this.usuario), httpOptions)
+      .pipe(map(res => res))
+      .subscribe(dados => console.log(dados));
+  }
 }
